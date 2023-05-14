06d6b560bf945ce9f7636a122a1b6ef4b217e96a
TaricOv <tareq.ouf@gmail.com>
2023-05-15 00:38:27 +0300

    added the manifesto page SVG

--- pages/manifesto.tsx
+++ pages/manifesto.tsx
@@ -1,21 +1,21 @@
 export default function Manifesto(){
     return(
         <>
-    <div className="flex items-start justify-center md:h-[50rem] h-[35rem]">
+    <div className="flex items-start justify-center md:h-[50rem] sm:h-[30rem]">
 
-    <div className="mt-10">
+    {/* <div className="mt-10 ">
         <div className="space-y-2 pb-8 md:space-y-5">
           <h1 className="opacity-[.04] text-[80px] font-extrabold leading-9 text-gray-900 dark:text-gray-100 sm:text-[80px] sm:leading-10 md:text-[100px] md:leading-14 uppercase">
             Manifesto
           </h1>
         </div>
-        </div>
-<div className="absolute top-1/2 left-1/2 transform -translate-x-2/4 -translate-y-2/4 w-2/3 md:top-2/3">
-    <svg className='-z-10 flex items-center justify-center text-center fill-red-500 w-30 h-30 ' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 262.95 262.48"><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><g id="_8ZU48m.tif" data-name="8ZU48m.tif"><path d="M33.06,134.62c-8.13,0-24,2-33.06,2,.64-8.67.59-16.34,1.92-23.76,1.69-9.42,4.54-18.63,6.83-27.94.76-3.07,2.54-4.59,5.7-5.31,9.87-2.23,19.67-4.79,29.48-7.24a7.91,7.91,0,0,0,4-2.07L16,68.38C41.16,27.15,76.29,2.71,123.94.22,175.84-2.49,215.68,20,245,64.16c-15.93,1.89-30.83.33-45.62,1.34,0,.69,0-.7,0,0,7.33.64,14.64,2.39,22,3.08,7.59.7,15.16,1.47,23.75,2.3-8.32,2.35-15.67,5.26-23.13,7.36.07.47-.07-.48,0,0h30c6.92,16.58,10.94,33.29,11.08,51.43-27,.82-53.68,2.26-80.31,3.07h0l78.7,14.57C257.59,197.6,213.87,255.81,142.69,262,73.41,268,13,218.51,2.7,153.4c21.39-2.77,42.39-6.34,63.41-9.06,0-.52,0,.52,0,0-16.86-1.05-44.28.57-61.5-.39,0-.22,9.79-2.92,9.75-3.14"/></g></g></g>
+        </div> */}
+<div className="absolute top-1/2 left-1/2 md:w-8/12 xl:w-6/12 transform -translate-x-2/4 -translate-y-2/4 w-2/3 md:top-2/3">
+    {/* <svg className='-z-10 flex items-center justify-center text-center fill-red-500 w-30 h-30 ' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 262.95 262.48"><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><g id="_8ZU48m.tif" data-name="8ZU48m.tif"><path d="M33.06,134.62c-8.13,0-24,2-33.06,2,.64-8.67.59-16.34,1.92-23.76,1.69-9.42,4.54-18.63,6.83-27.94.76-3.07,2.54-4.59,5.7-5.31,9.87-2.23,19.67-4.79,29.48-7.24a7.91,7.91,0,0,0,4-2.07L16,68.38C41.16,27.15,76.29,2.71,123.94.22,175.84-2.49,215.68,20,245,64.16c-15.93,1.89-30.83.33-45.62,1.34,0,.69,0-.7,0,0,7.33.64,14.64,2.39,22,3.08,7.59.7,15.16,1.47,23.75,2.3-8.32,2.35-15.67,5.26-23.13,7.36.07.47-.07-.48,0,0h30c6.92,16.58,10.94,33.29,11.08,51.43-27,.82-53.68,2.26-80.31,3.07h0l78.7,14.57C257.59,197.6,213.87,255.81,142.69,262,73.41,268,13,218.51,2.7,153.4c21.39-2.77,42.39-6.34,63.41-9.06,0-.52,0,.52,0,0-16.86-1.05-44.28.57-61.5-.39,0-.22,9.79-2.92,9.75-3.14"/></g></g></g>
   
     </svg>
     </div>
-    {/* <div className=""> */}
+    {/* <div className=""> 
     <div className="flex items-center justify-center text-center fill-red-500 w-30 h-30 absolute top-1/2 left-1/2 transform -translate-x-2/4 -translate-y-2/4 md:top-2/3">
     <ul className="md:!text-xl !text-[11px] !text-gray-300 list-decimal font-bold  dark:!text-gray-900 w-12/12 mx-auto dark:opacity-70 text-center select-none ">
     <li>Mistakes r spotlights on the road .. so we throw stones at them</li>
@@ -25,8 +25,8 @@ export default function Manifesto(){
     <li>Your citizenship is the proof your </li>
     <li>Being a member of the Samurai Nation means that u have gained </li>
     <li>By following me, you agree to never take anything I say seriously.</li>
-</ul>
-
+</ul> */}
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 531.24 530.3"><defs></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><g id="_8ZU48m.tif" data-name="8ZU48m.tif"><path className="cls-1" d="M369,268.17l159,29.44c-7.58,101.61-95.92,219.21-239.73,231.69-140,12.15-262-87.83-282.81-219.37,43.21-5.6,85.63-12.81,128.11-18.31-34.07-2.12-89.48,1.15-124.25-.79,0-.46,19.69-5.89,19.69-6.35L66.78,272c-16.42,0-48.56,4-66.78,4,1.3-17.51,1.18-33,3.87-48,3.43-19,9.18-37.64,13.81-56.45,1.53-6.21,5.12-9.28,11.52-10.73,19.93-4.52,39.72-9.68,59.56-14.63a16,16,0,0,0,8-4.18l-64.53-3.81C83.16,54.86,154.12,5.47,250.39.44,318.64-3.12,376.56,14.9,425,52.52q3.86,3,7.63,6.14,3.93,3.27,7.76,6.74,9,8.09,17.47,17.13,2.17,2.31,4.31,4.68,6.24,6.9,12.2,14.32,10.67,13.23,20.52,28.1c-1.63.19-3.26.37-4.88.53-17,1.67-33.46,1.54-49.62,1.45-12.68-.08-25.19-.14-37.65.71,12.55,1.11,25.1,3.83,37.65,5.46,2.24.29,4.47.55,6.71.76l6.28.59c13.26,1.26,26.72,2.61,41.7,4.08-16.82,4.74-31.66,10.61-46.73,14.86h60.51c14,33.5,22.09,67.25,22.38,103.92C476.68,263.63,422.79,266.55,369,268.17Z"/></g><text className="cls-2" transform="translate(52 208.63)">- Mistakes r spotlights on the road to get u back on track <tspan x="100.59" y="22.8">.. but we throw stones at them</tspan><tspan x="14.16" y="45.6">- We believe in the power of Coffee .. and Italian food.</tspan><tspan x="84.83" y="91.2">- You could fnd that we say things </tspan><tspan x="2.68" y="114">that contradict w/ what we could&apos;ve said before .. now u </tspan><tspan x="91.35" y="136.8">know that we r moody as shhhit!</tspan><tspan x="16.53" y="159.6">- If u r looking for humor, leave &apos;cuz we earn livin&apos; by </tspan><tspan x="155.5" y="182.4">being sarcastic. </tspan><tspan x="55.96" y="205.2">- By following me, you agree to never take </tspan><tspan x="117.62" y="228">anything we say seriously </tspan></text><g className="cls-3"><path className="cls-4" d="M177.52,52.52,165,113.66h-.25L152.41,52.52h-26v86.61h16V70h.25l15.4,69.14H171.7L187.11,70h.24v69.14h16V52.52Z"/><path className="cls-4" d="M254.31,52.52H232.72l-22.93,86.61H228l4-18.31h23l4,18.31h18.2ZM235,106.5l8.37-38.94h.24L252,106.5Z"/><path className="cls-4" d="M324.79,52.52v59.32h-.24L303.44,52.52H283.3v86.61h16V78.48h.25l21.35,60.65H340.8V52.52Z"/><path className="cls-4" d="M353.78,52.52v86.61h17.47V52.52Z"/><path className="cls-4" d="M432.63,58.66v8.18H401.7V87.21h29.11v14.32H401.7v37.6H384.23V52.52H425Z"/><path className="cls-4" d="M457.86,124.82V101.53h16.51q-6-7.43-12.2-14.32h-4.31V82.53q-8.5-9-17.47-17.13v66.21c16.16.09,32.61.22,49.62-1.45v-5.34Zm-17.47,13v1.35h13l-6.28-.59C444.86,138.33,442.63,138.07,440.39,137.78Z"/></g><text className="cls-5" transform="translate(126.08 155.34)">THE SAMURAI NATION</text></g></g></svg>
 </div>
     </div>
         </>
